e39d1fc84a114c545206a92a64a24834a2063a45
AmirNaderi1997 <83851066+AmirNaderi1997@users.noreply.github.com>
2025-10-01 20:30:54 +0330

    Create ERC-20_ABI.py

---
+++ ERC-20_ABI.py
@@ -0,0 +1,16 @@
+#If your token is a standard ERC-20 and you only want to support transfer()
+# you can use this minimal ABIi.
+
+contract_abi = [
+    {
+        "constant": False,
+        "inputs": [
+            {"name": "_to", "type": "address"},
+            {"name": "_value", "type": "uint256"}
+        ],
+        "name": "transfer",
+        "outputs": [{"name": "", "type": "bool"}],
+        "type": "function"
+    }
+]
+
