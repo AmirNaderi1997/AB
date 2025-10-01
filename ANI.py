c045c08193389e1b3789713a4f487d7d5fd52f3b
AmirNaderi1997 <83851066+AmirNaderi1997@users.noreply.github.com>
2025-10-01 20:29:43 +0330

    Create ABI.py

---
+++ ANI.py
@@ -0,0 +1,14 @@
+contract_abi = [
+    {
+        "constant": False,
+        "inputs": [
+            {"name": "_to", "type": "address"},
+            {"name": "_value", "type": "uint256"}
+        ],
+        "name": "transfer",
+        "outputs": [{"name": "", "type": "bool"}],
+        "type": "function"
+    }
+    # ...rest of ABI
+]
+
